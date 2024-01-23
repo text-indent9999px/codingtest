a10f404712593ee42146172c8ea6da775da2a941
text-indent9999px <106341032+text-indent9999px@users.noreply.github.com>
2024-01-24 02:04:06 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,10 +2,10 @@ function solution(brown, yellow) {
     let brownCase = [];
     for(let i=1; i<brown/2; i++){
         for(let j=1; j<=i+2; j++){
-            if(i+j === brown/2){
-                brownCase.push([i+2,j]);
+            if(i+j === brown/2 && (i)*(j-2) === yellow){
+                brownCase = [i+2,j];
             }
         }
     }
-    return brownCase.find((e => (e[0]-2)*(e[1]-2) === yellow));
+    return brownCase;
 }
