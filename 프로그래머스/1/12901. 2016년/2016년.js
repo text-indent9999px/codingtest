3ea44b6a1ce6074ed75120a471e5047b19c49aae
text-indent9999px <106341032+text-indent9999px@users.noreply.github.com>
2024-01-26 11:09:46 +0900

    [level 1] Title: 2016년, Time: 0.35 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,27 +1,4 @@
 function solution(a, b) {
-    let result = new Date('2016-'+ a +'-' + b).getDay();
-    switch(result){
-        case 0 : 
-            result = 'SUN';
-            break;
-        case 1 : 
-            result = 'MON';
-            break;
-        case 2 :
-            result = 'TUE';
-            break;
-        case 3 :
-            result = 'WED';
-            break;
-        case 4 :
-            result = 'THU';
-            break;
-        case 5 :
-            result = 'FRI';
-            break;
-        case 6 :
-            result = 'SAT';
-            break;
-    }
+    let result = new Date('2016-'+ a +'-' + b).toString().slice(0,3).toUpperCase();
     return result;
 }
