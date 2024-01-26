c9449842e0a6216414906c892b5220d1843607ef
text-indent9999px <106341032+text-indent9999px@users.noreply.github.com>
2024-01-26 19:04:19 +0900

    [level 1] Title: 모의고사, Time: 8.11 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,30 +1,21 @@
 function solution(answers) {
-    this.sadMan1 = (idx) => idx%5+1;
-    this.sadMan2 = (idx) => {
-        let points = [1,3,4,5];
-        return (idx % 2 === 0) ? 2 : points[Math.floor((idx - Math.floor(idx/8)*8)/2)]
-    };
-    this.sadMan3 = (idx) => {
-        let points = [3,1,2,4,5];
-        return points[ Math.floor(String(idx).slice(-1)/2) ];
-    }
-    let compareFunc = (f) => {
-        return answers.reduce((acc,cur,idx)=>{
-            if(cur === f(idx)){
-                acc++;
+    this.man1 = [1, 2, 3, 4, 5];
+    this.man2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    this.man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let count = [0,0,0];
+    for(let i = 0; i < answers.length; i++) {
+        for(let j = 1; j<=3; j++){
+            let length = this['man'+j].length;
+            if(answers[i] === this['man'+j][i % length]){
+                count[j-1]++;
             }
-            return acc;
-        },0);
-    }
-    let [result, compare] = [[], 0];
-    for(let i=0; i<3; i++){
-        let count = compareFunc(this['sadMan'+(i+1)]);
-        if(count > compare){
-            result = [i+1];
-            compare = count;
-        }else if(count == compare){
-            result.push(i+1);
         }
     }
-    return result;
+    let max = Math.max(...count);
+    return count.reduce((acc,cur,idx)=>{
+        if(cur >= max){
+            acc.push(idx+1);
+        }
+        return acc;
+    },[]);
 }
